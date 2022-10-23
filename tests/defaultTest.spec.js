410980506cfd3d138c0fd7b56562e917211924d3
kkamara <kelvinkamara@protonmail.com>
2022-10-23 22:06:40 +0100

    Add google test

--- tests/defaultTest.spec.js
+++ tests/defaultTest.spec.js
@@ -2,10 +2,10 @@ module.exports = {
   '@tags': ['beta'],
   'NW test on Google' : function (client) {
     client
+      .useXpath()
       .url('http://www.google.com')
-      .waitForElementVisible('body', 1000)
       .assert.title('Google')
-      .pause(1000)
+      .pause(5000)
       .end()
   }
 }
