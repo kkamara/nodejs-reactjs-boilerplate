bdfba3f9a4bbea2b467e4394f110d6ead5bf0309
kkamara <kamaracomputers@gmail.com>
2025-04-28 02:56:54 +0100

    Update allowed CORS methods

--- src/app.js
+++ src/app.js
@@ -88,7 +88,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', config.appURL+':'+config.appPort);
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization, x-id, Content-Length, X-Requested-With');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
     next();
 });
 
