a81bc1082a101dedafafe7ea3392f855a1c70bc2
kkamara <kamaracomputers@gmail.com>
2025-05-15 18:02:06 +0100

    Copy in-house API routes to make public API routes at /api/v1

--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -1,9 +1,11 @@
 'use strict';
 const express = require('express');
 const web = require("./web");
+const user = require("./user");
 
 const router = express.Router();
 
 router.use("/web", web);
+router.use("/user", user);
 
 module.exports = router;
