5ef8f51bc8203cbb2cd38e6bcb54ad25601126e8
kkamara <kamaracomputers@gmail.com>
2025-05-25 23:28:41 +0100

    Add demo-user for automatic login to ReactJS app

--- src/seeders/v1/20230814135938-demo-users.js
+++ src/seeders/v1/20230814135938-demo-users.js
@@ -9,19 +9,31 @@ const { encrypt, } = require("../../utils/tokens");
 module.exports = {
   up: (queryInterface, Sequelize) => {
     const fakeUsers = [];
+    let pwd;
 
     for(let i=0; i < 31; i++) {
-      const { hash, salt, } = encrypt("secret");
+      pwd = encrypt("secret");
       fakeUsers.push({
         email: faker.internet.email(),
         firstName: faker.person.firstName(),
         lastName: faker.person.lastName(),
-        password: hash,
-        passwordSalt: salt,
+        password: pwd.hash,
+        passwordSalt: pwd.salt,
         createdAt: moment().tz(appTimezone).format(mysqlTimeFormat),
         updatedAt: moment().tz(appTimezone).format(mysqlTimeFormat),
       })
     }
+    
+    pwd = encrypt("secret");
+    fakeUsers.push({
+      email: "jane@doe.com",
+      firstName: "Jane",
+      lastName: "Doe",
+      password: pwd.hash,
+      passwordSalt: pwd.salt,
+      createdAt: moment().tz(appTimezone).format(mysqlTimeFormat),
+      updatedAt: moment().tz(appTimezone).format(mysqlTimeFormat),
+    })
 
     return queryInterface.bulkInsert('users', [
       ...fakeUsers,
