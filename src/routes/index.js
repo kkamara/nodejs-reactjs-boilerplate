6abe1d8f684ed1847af5545825ef324c1da4a6f4
kkamara <kamaracomputers@gmail.com>
2025-05-31 13:46:49 +0100

    Add V1 mobile API route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const express = require('express');
 const home = require('./home');
 const api = require('./api');
 const v1 = require('./api/v1');
+const mobileV1 = require('./api/mobile/v1');
 
 const router = express.Router();
 
 router.use('/api', api);
 router.use('/api/v1', v1);
+router.use('/mobile-api/v1', mobileV1);
 router.use('/home', home);
 
 module.exports = router;
