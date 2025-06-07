92bb11a91a5b4b3a6dc0bd81c146f80203cef130
kkamara <kamaracomputers@gmail.com>
2025-06-07 14:57:19 +0100

    Improve layout of 404 Not Found Screen

--- frontend/src/components/pages/http/NotFoundComponent.jsx
+++ frontend/src/components/pages/http/NotFoundComponent.jsx
@@ -4,7 +4,7 @@ import "./NotFoundComponent.scss"
 
 export default function NotFoundComponent() {
   return (
-    <div>
+    <div className="mt-5 col-md-4 offset-md-4">
       <h1 className="not-found-lead fw-bold">
         Oops, the page you have requested has not been found.
       </h1>
