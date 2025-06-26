8d6eddc99787f8ed65dbee9b9d10c47c4854bc3e
kkamara <kamaracomputers@gmail.com>
2025-06-26 11:28:56 +0100

    Convert user database seeder to a transaction

--- src/seeders/v1/20230814135938-demo-users.js
+++ src/seeders/v1/20230814135938-demo-users.js
@@ -2,21 +2,38 @@
 const moment = require("moment-timezone");
 const { faker, } = require('@faker-js/faker');
 const { mysqlTimeFormat, } = require("../../utils/time");
-const { appTimezone, } = require("../../config/index");
 const { encrypt, } = require("../../utils/tokens");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    const fakeUsers = [];
-    let pwd;
+  up: async (queryInterface, Sequelize) => {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      const fakeUsers = [];
+      let pwd;
 
-    for(let i=0; i < 31; i++) {
+      for(let i=0; i < 30; i++) {
+        pwd = encrypt("secret");
+        fakeUsers.push({
+          email: faker.internet.email(),
+          firstName: faker.person.firstName(),
+          lastName: faker.person.lastName(),
+          password: pwd.hash,
+          passwordSalt: pwd.salt,
+          createdAt: moment()
+            .utc()
+            .format(mysqlTimeFormat),
+          updatedAt: moment()
+            .utc()
+            .format(mysqlTimeFormat),
+        })
+      }
+      
       pwd = encrypt("secret");
       fakeUsers.push({
-        email: faker.internet.email(),
-        firstName: faker.person.firstName(),
-        lastName: faker.person.lastName(),
+        email: "jane@doe.com",
+        firstName: "Jane",
+        lastName: "Doe",
         password: pwd.hash,
         passwordSalt: pwd.salt,
         createdAt: moment()
@@ -26,28 +43,24 @@ module.exports = {
           .utc()
           .format(mysqlTimeFormat),
       })
-    }
-    
-    pwd = encrypt("secret");
-    fakeUsers.push({
-      email: "jane@doe.com",
-      firstName: "Jane",
-      lastName: "Doe",
-      password: pwd.hash,
-      passwordSalt: pwd.salt,
-      createdAt: moment()
-        .utc()
-        .format(mysqlTimeFormat),
-      updatedAt: moment()
-        .utc()
-        .format(mysqlTimeFormat),
-    })
 
-    return queryInterface.bulkInsert('users', [
-      ...fakeUsers,
-    ]);
+      await queryInterface.bulkInsert('users', [
+        ...fakeUsers,
+      ], { transaction, });
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
   },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('users', null, {});
+  down: async (queryInterface, Sequelize) => {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      queryInterface.bulkDelete('users', null, {});
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw err;
+    }
   }
 };
