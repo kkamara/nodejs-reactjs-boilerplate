0df5d7b3aff474a422b7e5298400fda817faa26d
kkamara <kamaracomputers@gmail.com>
2025-07-12 19:52:45 +0100

    Refactor AuthRoute

--- frontend/src/AuthRoute.jsx
+++ frontend/src/AuthRoute.jsx
@@ -1,22 +1,24 @@
 import React, { useEffect, } from "react"
 import { Outlet, } from "react-router"
 import { useSelector, } from "react-redux"
+import { Navigate, } from "react-router"
 
 const AuthRoute = ({ redirectPath, }) => {
   const state = useSelector(state => ({
     auth: state.auth,
   }))
 
-  useEffect(() => {
-    if (state.auth.error || null === localStorage.getItem("user-token")) {
+  const userStorage = localStorage.getItem("user-token")
+  if (state.auth.error || null === userStorage) {
+    if (null !== userStorage) {
       localStorage.removeItem("user-token")
-      if (redirectPath) {
-        window.location.href = redirectPath
-      } else {
-        window.location.href = "/user/login"
-      }
     }
-  }, [state.auth])
+    if (redirectPath) {
+      return <Navigate to={redirectPath}/>
+    } else {
+      return <Navigate to={"/user/login"}/>
+    }
+  }
 
   return <Outlet/>
 }
