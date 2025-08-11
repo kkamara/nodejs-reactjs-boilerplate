5dc6e41fa6d073e45d522d6536833bfaf36a94bc
kkamara <kamaracomputers@gmail.com>
2025-08-11 18:07:36 +0100

    Add regex for number with optional decimal part

--- src/utils/regexes.js
+++ src/utils/regexes.js
@@ -1,3 +1,4 @@
 'use strict';
 exports.integerNumberRegex = /^\d+$/;
 exports.validEmailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+exports.numberWithOptionalDecimalPartRegex = /^\d+(?:[.,]\d+)?$/;
