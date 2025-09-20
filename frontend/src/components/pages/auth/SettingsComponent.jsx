1965ab8dd634bb75e52db1bfbb207296e26edf18
kkamara <kamaracomputers@gmail.com>
2025-09-20 18:47:02 +0100

    Add Redux to settings screen

--- frontend/src/components/pages/auth/SettingsComponent.jsx
+++ frontend/src/components/pages/auth/SettingsComponent.jsx
@@ -1,13 +1,31 @@
 import React from "react"
 import { Helmet, } from "react-helmet"
+import { useSelector, useDispatch } from "react-redux"
 
 export default function SettingsComponent() {
+  const state = useSelector(state => ({
+    auth: state.auth,
+  }))
+  const dispatch = useDispatch()
+
+  if (state.auth.loading) {
+    <div className="container settings-container">
+      <Helmet>
+        <title>User Settings - {process.env.REACT_APP_NAME}</title>
+      </Helmet>
+    </div>
+  }
+
   return (
     <div className="container settings-container">
       <Helmet>
-        <title>Settings - {process.env.REACT_APP_NAME}</title>
+        <title>User Settings - {process.env.REACT_APP_NAME}</title>
       </Helmet>
-      <div>SettingsComponent</div>
+      <div className="row">
+        <div className="col-md-4 offset-md-4">
+          <h1>User Settings</h1>
+        </div>
+      </div>
     </div>
   )
 }
