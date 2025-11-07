55156c0bb2962d4b1fa60c8da004f0a348f38f18
kkamara <kamaracomputers@gmail.com>
2025-11-07 19:27:56 +0000

    Add missing async declaration

--- src/controllers/API/healthControllers.js
+++ src/controllers/API/healthControllers.js
@@ -4,7 +4,7 @@ const { status, } = require("http-status");
 const { message200, } = require('../../utils/httpResponses');
 const asyncHandler = require("express-async-handler");
 
-const health = asyncHandler((req, res) => {
+const health = asyncHandler(async (req, res) => {
   res.status(status.OK);
   return res.json({
     message: message200,
