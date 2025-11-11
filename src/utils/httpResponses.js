4c5b49003ff1a81f9462349cfef8970776ab9950
kkamara <kamaracomputers@gmail.com>
2025-11-11 13:22:58 +0000

    Rename authorize to authorise (UK spelling)

--- src/utils/httpResponses.js
+++ src/utils/httpResponses.js
@@ -2,5 +2,5 @@
 exports.message404 = "Resource not found.";
 exports.message200 = "Success";
 exports.message400 = "Unable to process the input you have provided.";
-exports.message401 = "Unauthorized";
+exports.message401 = "Unauthorised";
 exports.message500 = "There was an unknown error. Please contact an administrator.";
