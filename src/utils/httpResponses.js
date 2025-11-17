d10703559f26a9d4e5e4c81e400942df24dd0b49
kkamara <kamaracomputers@gmail.com>
2025-11-17 13:18:55 +0000

    Add periods to HTTP responses where required

--- src/utils/httpResponses.js
+++ src/utils/httpResponses.js
@@ -1,6 +1,6 @@
 'use strict';
 exports.message404 = "Resource not found.";
-exports.message200 = "Success";
+exports.message200 = "Success.";
 exports.message400 = "Unable to process the input you have provided.";
-exports.message401 = "Unauthorised";
+exports.message401 = "Unauthorised.";
 exports.message500 = "There was an unknown error. Please contact an administrator.";
