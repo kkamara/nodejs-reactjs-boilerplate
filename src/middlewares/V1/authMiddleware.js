c7643d2e81ddf979d4032be22a32343d73237ca3
kkamara <kamaracomputers@gmail.com>
2025-12-02 17:21:40 +0000

    Throw errors in auth middlewares

--- src/middlewares/V1/authMiddleware.js
+++ src/middlewares/V1/authMiddleware.js
@@ -3,13 +3,12 @@ const { status, } = require("http-status");
 const moment = require("moment-timezone");
 const { message401, } = require("../../utils/httpResponses");
 const db = require("../../models/V1/index");
+const asyncHandler = require("express-async-handler");
 
-module.exports.authenticate = async (req, res, next) => {
+module.exports.authenticate = asyncHandler(async (req, res, next) => {
   if (!req.headers.authorization) {
     res.status(status.UNAUTHORIZED);
-    return res.json({
-      message: message401,
-    });
+    throw new Error(message401);
   }
   const authTokenResult = await db.sequelize
     .models
@@ -19,21 +18,17 @@ module.exports.authenticate = async (req, res, next) => {
     );
   if (false === authTokenResult) {
     res.status(status.UNAUTHORIZED);
-    return res.json({
-      message: message401,
-    });
+    throw new Error(message401);
   }
   const tokenIsExpired = moment(authTokenResult.expiredAt)
     .unix() < moment().utc().unix();
   if (true === tokenIsExpired) {
     res.status(status.UNAUTHORIZED);
-    return res.json({
-      message: message401,
-    });
+    throw new Error(message401);
   }
   const extractedToken = req.headerString("authorization")
     .split(" ")[1];
   req.session.userId = authTokenResult.userId;
   req.session.extractedToken = extractedToken;
   return next();
-};
+});
