cbd3a9bdb63046bbf6e81a674246cccd1b216971
kkamara <kamaracomputers@gmail.com>
2025-12-04 16:32:20 +0000

    Update throttle max requests

--- src/middlewares/V1/throttleMiddleware.js
+++ src/middlewares/V1/throttleMiddleware.js
@@ -4,7 +4,7 @@ const config = require('../../config');
 
 module.exports.limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // Every 15 minutes
-  limit: "production" === config.nodeEnv ? 100 : 10000,
+  limit: "production" === config.nodeEnv ? 10000 : 100000,
   standardHeaders: 'draft-8',
   legacyHeaders: false,
   ipv6Subnet: 56,
